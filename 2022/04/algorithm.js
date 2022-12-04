0522289fce776112a0823dc786c7ba3e543814e7
Manuel Martin <manuel.martin@gmail.com>
2022-12-04 16:19:51 +0100

    refactor: day 04

--- 2022/04/algorithm.js
+++ 2022/04/algorithm.js
@@ -1,5 +1,5 @@
 const getAssignmentRangePair = assignmentRangePair => assignmentRangePair.split(',')
-  .map((assignmentRange) => getAssignmentRange(assignmentRange));
+  .map(getAssignmentRange);
 
 const getAssignmentRange = assignmentRange => assignmentRange.split('-')
   .map((item) => parseInt(item, 10));
@@ -10,9 +10,9 @@ const checkIfOneRangeFullyContainsTheOther = ([first, second]) =>
   checkIfFirstRangeFullyContainsTheOther(first, second) || checkIfFirstRangeFullyContainsTheOther(second, first);
 
 export const totalOfAssignmentsThatOneRangeFullyContainsTheOther = assignmentRangePairs => assignmentRangePairs
-  .map(ranges => getAssignmentRangePair(ranges))
-  .map(ranges => checkIfOneRangeFullyContainsTheOther(ranges))
-  .reduce((total, rangeFullyContainsTheOther) => rangeFullyContainsTheOther ? total += 1 : total);
+  .map(getAssignmentRangePair)
+  .map(checkIfOneRangeFullyContainsTheOther)
+  .reduce((total, rangeFullyContainsTheOther) => rangeFullyContainsTheOther ? total + 1 : total);
 
 const checkIfFirstRangeOverlapsTheOther = ([a1, a2], [b1, b2]) => (a1 <= b1 && a2 >= b1) || (a1 <= b2 && a2 >= b2);
 
@@ -20,6 +20,6 @@ const checkIfRangesOverlap = ([first, second]) =>
   checkIfFirstRangeOverlapsTheOther(first, second) || checkIfFirstRangeOverlapsTheOther(second, first);
 
 export const totalOfAssignmentsThatRangesOverlap = assignmentRangePairs => assignmentRangePairs
-  .map(ranges => getAssignmentRangePair(ranges))
-  .map(ranges => checkIfRangesOverlap(ranges))
-  .reduce((total, rangeFullyContainsTheOther) => rangeFullyContainsTheOther ? total += 1 : total);
+  .map(getAssignmentRangePair)
+  .map(checkIfRangesOverlap)
+  .reduce((total, rangeFullyContainsTheOther) => rangeFullyContainsTheOther ? total + 1 : total);
